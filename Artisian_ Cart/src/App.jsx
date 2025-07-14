19517b13fa9610139685f0f150e9c907accf5d41
DishantSarangal <sarangaldishant01@gmail.com>
2025-07-14 19:57:50 +0530

    Added Artisian_Cart with Client frontend

---
+++ Artisian_ Cart/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navigation from './components/navigation';
+import './App.css';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <div>
+      <Navigation />
+      <Home/>
+      
+    </div>
+  );
+}
+
+export default App;
