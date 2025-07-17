2ec2abc7fc9767ad3e96e6185b758657f765c4f9
OnkarSingh24 <onkar.s2414@gmail.com>
2025-07-17 22:42:10 +0530

    Update App.jsx

--- Frontened/src/App.jsx
+++ Frontened/src/App.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import Navigation from './components/navigation';
 import './App.css';
+import Home from './components/Home';
 import Artisians from './components/artisians';
-import ArtisanDirectory from './components/artisandirectory';
 import SignIn from './components/login';
 import RegisterAsSeller from './components/Registerasseller';
 import ResetPassword from './components/resetpassword';
@@ -16,8 +16,8 @@ function AppContent() {
     <div className="app">
       {!hideNavbarPaths.includes(location.pathname) && <Navigation />}
       <Routes>
-        <Route path="/" element={<Artisians />} />
-        <Route path="/artisans" element={<ArtisanDirectory />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/artisans" element={<Artisians />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/registerasseller" element={<RegisterAsSeller />} />
         <Route path="/resetpassword" element={<ResetPassword />} /> 
