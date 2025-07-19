acaf58dffd9905589329bfab480aeaba327c52d2
OnkarSingh24 <onkar.s2414@gmail.com>
2025-07-19 13:31:33 +0530

    Update App.jsx

--- Frontened/src/App.jsx
+++ Frontened/src/App.jsx
@@ -1,26 +1,46 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import Navigation from './components/navigation';
-import './App.css';
 import Home from './components/Home';
 import Artisians from './components/artisians';
-import SignIn from '../../client/src/pages/Login';
+import SignIn from './components/Login'; // This is your Login component
 import RegisterAsSeller from './components/Registerasseller';
 import ResetPassword from './components/resetpassword';
+import ArtisanDirectory from './components/ArtisanDirectory';
+import Register from './components/Register'; // This is your Register component
+
+import './App.css';
 
 function AppContent() {
   const location = useLocation();
-  const hideNavbarPaths = ['/signin', '/registerasseller', '/resetpassword'];
+  const hideNavbarPaths = ['/signin', '/registerasseller', '/resetpassword', '/Register'];
+  const showNavbar = !hideNavbarPaths.includes(location.pathname);
+
+  useEffect(() => {
+    const navHeight = 64; 
+
+    if (showNavbar) {
+      document.body.style.paddingTop = `${navHeight}px`;
+    } else {
+      document.body.style.paddingTop = '0';
+    }
+
+    return () => {
+      document.body.style.paddingTop = '0';
+    };
+  }, [showNavbar]);
 
   return (
     <div className="app">
-      {!hideNavbarPaths.includes(location.pathname) && <Navigation />}
+      {showNavbar && <Navigation />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/artisans" element={<Artisians />} />
-        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signin" element={<SignIn />} /> {/* This route is correct for Login.jsx */}
         <Route path="/registerasseller" element={<RegisterAsSeller />} />
-        <Route path="/resetpassword" element={<ResetPassword />} /> 
+        <Route path="/resetpassword" element={<ResetPassword />} />
+        <Route path="/artisandirectory" element={<ArtisanDirectory />} />
+        <Route path="/Register" element={<Register />} />
       </Routes>
     </div>
   );
