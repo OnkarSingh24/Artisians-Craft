019a64eaf25f66b001e41368850371358350bba2
sharmakshita <sharmakshita42@gmail.com>
2025-09-25 19:07:17 +0530

    A

--- Frontened/src/Components/Resetpassword.jsx
+++ Frontened/src/Components/Resetpassword.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from 'react';
-import './ResetPassword.css';
+import './Resetpassword.css';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { content } from '../../context';
